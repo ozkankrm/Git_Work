583e99a16880bcb3de2fc03bd1c02061f3495500
mustafa3342 <72839564+mustafa3342@users.noreply.github.com>
2021-02-19 01:12:27 +0000

    added new line classmus

--- src/test/java/Class4.java
+++ src/test/java/Class4.java
@@ -1,11 +1,9 @@
 public class Class4 {
-
     public static void main(String[] args) {
         System.out.println("Hello");
         //Add class4
 
         // who did add this line
     }
-
 
 }
---
+++ src/test/java/mus.java
@@ -0,0 +1,3 @@
+public class mus {
+    // don't erase this class
+}
